e7e7354d2913f492a911c9425b2106d2718ff8bb
Roman Valero <valeroman@gmail.com>
2023-01-05 16:02:50 -0500

    deploy

--- README.md
+++ README.md
@@ -19,7 +19,16 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
-5. Reconstruir la base de datos con la semilla
+5. Clonar el archivo ```.env.template``` y renombrar la copia a ```.env```
+
+6. Llenar la variables de entorno definidas en ```.env```
+
+7. Ejecutar la aplicación en dev: 
+```
+yarn start:dev
+```
+
+8. Reconstruir la base de datos con la semilla
 ```
 http://localhost:3000/api/v2/seed
 ```
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,5 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
 
@@ -11,12 +12,18 @@ import { PaginationDto } from '../common/dto/pagination.dto';
 @Injectable()
 export class PokemonService {
 
+  private defaulLimit: number;
+
   constructor(
 
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>,
 
-  ) {}
+    private readonly configService: ConfigService,
+
+  ) {
+    this.defaulLimit = configService.get<number>('defaultLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -34,7 +41,7 @@ export class PokemonService {
 
   findAll( paginationDto: PaginationDto ) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaulLimit, offset = 0 } = paginationDto;
     return this.pokemonModel.find()
       .limit( limit )
       .skip( offset )
